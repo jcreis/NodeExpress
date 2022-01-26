dd5dc7b83e274a4bba658886e18bd142e54e5352
jcreis <jc.reis@campus.fct.unl.pt>
2022-01-26 16:29:46 +0000

    added POST request -> tested

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const courses = [
     {id: 1, name: 'course1'},
     {id: 2, name: 'course2'},
     {id: 3, name: 'course3'},
 ];
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!!');
@@ -23,8 +28,19 @@ app.get('/api/courses/:id', (req, res) => {
     else{
         res.send(course);
     }
-        
 });
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+
+
 
 const port = process.env.PORT || 3000;
 
